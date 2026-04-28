0a9f33fcdbfe1350027d5f00b0b026cf1daa94f4
unknown <Professor@SN-396733.edu.fiesc.com.br>
2026-04-28 10:41:02 -0300

    register service orker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './utils/reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
